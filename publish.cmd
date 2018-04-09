869e8d2e8e66a00161350e3f77d5d036e2fc133f
Prenzler <mprenzler@viainfo.net>
2018-04-09 14:34:31 -0500

    'test'

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=viafaq-Web-Deploy.pubxml -p:Password=CJRsybQCJo3drQqHdDAQ5rYk3Mwj1qrB2nHcnqdqiSq6eda0ar4ZwaCQ6367
+
